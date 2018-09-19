9dd26dbe570768d8f5ebd02564d9a692b7056a89
PC <PC@192.168.0.4>
2018-09-19 18:14:57 +0530

    jenkin comitt suiside

---
+++ JenkinPro/file.bat
@@ -0,0 +1,1 @@
+java -cp bin;library/* org.testng.TestNG testNG.xml
---
+++ JenkinPro/src/demo/Demo.java
@@ -0,0 +1,24 @@
+package demo;
+
+import java.io.File;
+
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo 
+{
+	@Test
+	public void m()
+	{
+		 System.setProperty("webdriver.chrome.driver", "C:\\kiran selinium\\chromedriver.exe");
+		  WebDriver driver = new ChromeDriver();
+		  driver.get("https://www.seleniumhq.org/download/");
+		  driver.manage().window().maximize();
+		  TakesScreenshot ts = (TakesScreenshot) driver;
+		  File f =new File ("ts");
+		  
+	}
+
+}
